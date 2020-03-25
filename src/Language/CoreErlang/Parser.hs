b2cf725a9969e78e118bae00a1963f6ebb983932
Feng Lee <feng@emqx.io>
2020-03-25 08:05:18 +0800

    Fix the do layout

--- src/Language/CoreErlang/Parser.hs
+++ src/Language/CoreErlang/Parser.hs
@@ -43,41 +43,46 @@ namechar = uppercase <|> lowercase <|> digit <|> oneOf "@_"
 -- Terminals
 
 integer :: Parser Integer
-integer = do i <- positive <|> negative <|> decimal
-             whiteSpace -- TODO: buff
-             return $ i
+integer = do
+  i <- positive <|> negative <|> decimal
+  whiteSpace -- TODO: buff
+  return $ i
 
 positive :: Parser Integer
-positive = do char '+'
-              p <- decimal
-              return p
+positive = do
+  char '+'
+  p <- decimal
+  return p
 
 negative :: Parser Integer
-negative = do char '-'
-              n <- decimal
-              return $ negate n
+negative = do
+  char '-'
+  n <- decimal
+  return $ negate n
 
 atom :: Parser Atom
-atom = do char '\''
-          a <- many (noneOf "\n\r\\\'")
-          char '\''
-          whiteSpace -- TODO: buff
-          return $ Atom a
+atom = do
+  char '\''
+  a <- many (noneOf "\r\n\\\'")
+  char '\''
+  whiteSpace -- TODO: buff
+  return $ Atom a
 
 echar :: Parser Literal
-echar = do char '$'
-           c <- noneOf "\n\r\\ "
-           whiteSpace -- TODO: buff
-           return $ LChar c
+echar = do
+  char '$'
+  c <- noneOf "\n\r\\ "
+  whiteSpace -- TODO: buff
+  return $ LChar c
 
 estring :: Parser Literal
-estring = do char '"'
-             s <- many $ noneOf "\n\r\\\""
-             char '"'
-             return $ LString s
+estring = do
+  char '"'
+  s <- many $ noneOf "\n\r\\\""
+  char '"'
+  return $ LString s
 
 variable :: Parser Var
--- variable = (uppercase | (_ namechar)) namechar*
 variable = identifier
 
 -- Non-terminals
@@ -86,42 +91,50 @@ emodule :: Parser (Ann Module)
 emodule = annotated amodule
 
 amodule :: Parser Module
-amodule = do reserved "module"
-             name <- atom
-             funs <- exports
-             attrs <- attributes
-             fundefs <- many fundef
-             reserved "end"
-             return $ Module name funs attrs fundefs
+amodule = do
+  reserved "module"
+  name <- atom
+  funs <- exports
+  attrs <- attributes
+  fundefs <- many fundef
+  reserved "end"
+  return $ Module name funs attrs fundefs
 
 exports :: Parser [FunName]
 exports = brackets $ commaSep function
 
-attributes :: Parser [(Atom, Const)]
-attributes = do reserved "attributes"
-                brackets (commaSep $ do a <- atom
-                                        symbol "="
-                                        c <- constant
-                                        return (a,c))
+attributes :: Parser [(Atom,Const)]
+attributes = do
+  reserved "attributes"
+  brackets $ commaSep attribute
+
+attribute :: Parser (Atom,Const)
+attribute = do
+  a <- atom
+  symbol "="
+  c <- constant
+  return (a,c)
 
 constant :: Parser Const
 constant = liftM CLit (try literal) <|>
            liftM CTuple (tuple constant) <|>
            liftM CList (elist constant) <|>
-           liftM CMap (emap constant constant)
+           liftM CMap (emap "=>" constant constant)
 
 fundef :: Parser FunDef
-fundef = do name <- annotated function
-            symbol "="
-            body <- annotated lambda
-            return $ FunDef name body
+fundef = do
+  name <- annotated function
+  symbol "="
+  body <- annotated lambda
+  return $ FunDef name body
 
 function :: Parser FunName
-function = do a <- atom
-              char '/'
-              i <- decimal
-              whiteSpace -- TODO: buff
-              return $ FunName (a, i)
+function = do
+  a <- atom
+  char '/'
+  i <- decimal
+  whiteSpace -- TODO: buff
+  return $ FunName (a,i)
 
 literal :: Parser Literal
 literal = try (liftM LFloat float) <|> liftM LInt integer <|>
@@ -141,70 +154,66 @@ sexpression = app <|> ecatch <|> ecase <|> elet <|>
               liftM List (try $ elist expression) {- because of nil -} <|>
               liftM Lit literal <|> modcall <|> op <|> receive <|>
               eseq <|> etry <|> liftM Tuple (tuple expression) <|>
-              liftM EMap (emap expression expression) <|>
+              liftM EMap (emap "=>" expression expression) <|>
               liftM Var variable
 
 app :: Parser Expr
-app = do reserved "apply"
-         e1 <- expression
-         eN <- parens $ commaSep expression
-         return $ App e1 eN
+app = do
+  reserved "apply"
+  e1 <- expression
+  eN <- parens $ commaSep expression
+  return $ App e1 eN
 
 ecatch :: Parser Expr
-ecatch = do reserved "catch"
-            e <- expression
-            return $ Catch e
+ecatch = do
+  reserved "catch"
+  e <- expression
+  return $ Catch e
 
 ebinary :: Parser a -> Parser [Bitstring a]
-ebinary p = do symbol "#"
-               bs <- braces (commaSep (bitstring p))
-               symbol "#"
-               return bs
+ebinary p = do
+  symbol "#"
+  bs <- braces (commaSep (bitstring p))
+  symbol "#"
+  return bs
 
-emap :: Parser k -> Parser v -> Parser (Map k v)
-emap kp vp = do symbol "~"
-                l <- braces (commaSep (emapKV kp vp))
-                symbol "~"
-                return $ Map l
+emap :: String -> Parser k -> Parser v -> Parser (Map k v)
+emap s kp vp = do
+  symbol "~"
+  l <- braces (commaSep (emapKV s kp vp))
+  symbol "~"
+  return $ Map l
 
-emapKV :: Parser k -> Parser v -> Parser (k,v)
-emapKV kp vp = do k <- kp
-                  symbol "=>"
-                  v <- vp
-                  return (k,v)
-
-pmap :: Parser k -> Parser v -> Parser (Map k v)
-pmap kp vp = do symbol "~"
-                l <- braces (commaSep (pmapKV kp vp))
-                symbol "~"
-                return $ Map l
-
-pmapKV :: Parser k -> Parser v -> Parser (k,v)
-pmapKV kp vp = do k <- kp
-                  symbol ":="
-                  v <- vp
-                  return (k,v)
+emapKV :: String -> Parser k -> Parser v -> Parser (k,v)
+emapKV s kp vp = do
+  k <- kp
+  symbol s
+  v <- vp
+  return (k,v)
 
 bitstring :: Parser a -> Parser (Bitstring a)
-bitstring p = do symbol "#"
-                 e0 <- angles p
-                 es <- parens (commaSep expression)
-                 return $ Bitstring e0 es
+bitstring p = do
+  symbol "#"
+  e0 <- angles p
+  es <- parens (commaSep expression)
+  return $ Bitstring e0 es
 
 ecase :: Parser Expr
-ecase = do reserved "case"
-           e <- expression
-           reserved "of"
-           alts <- many1 (annotated clause)
-           reserved "end"
-           return $ Case e alts
+ecase = do
+  reserved "case"
+  e <- expression
+  reserved "of"
+  alts <- many1 (annotated clause)
+  reserved "end"
+  return $ Case e alts
 
 clause :: Parser Alt
-clause = do pat <- patterns
-            g <- guard
-            symbol "->"
-            e <- expression
-            return $ Alt pat g e
+clause = do
+  pat <- patterns
+  g <- guard
+  symbol "->"
+  e <- expression
+  return $ Alt pat g e
 
 patterns :: Parser Pats
 patterns = liftM Pat pattern <|>
@@ -214,110 +223,123 @@ pattern :: Parser Pat
 pattern = liftM PAlias (try alias) {- because of variable -} <|> liftM PVar variable <|>
           liftM PLit (try literal) {- because of nil -} <|> liftM PTuple (tuple pattern) <|>
           liftM PList (elist pattern) <|> liftM PBinary (ebinary pattern) <|>
-          liftM PMap (pmap pkey pattern) -- TODO: Fixme later
+          liftM PMap (emap ":=" pkey pattern) -- TODO: Fixme later
 
 pkey :: Parser Key
 pkey = liftM KVar variable <|> liftM KLit literal
 
 alias :: Parser Alias
-alias = do v <- variable
-           symbol "="
-           p <- pattern
-           return $ Alias v p
+alias = do
+  v <- variable
+  symbol "="
+  p <- pattern
+  return $ Alias v p
 
 guard :: Parser Guard
-guard = do reserved "when"
-           e <- expression
-           return $ Guard e
+guard = do
+  reserved "when"
+  e <- expression
+  return $ Guard e
 
 elet :: Parser Expr
-elet = do reserved "let"
-          vars <- variables
-          symbol "="
-          e1 <- expression
-          symbol "in"
-          e2 <- expression
-          return $ Let (vars,e1) e2
+elet = do
+  reserved "let"
+  vars <- variables
+  symbol "="
+  e1 <- expression
+  symbol "in"
+  e2 <- expression
+  return $ Let (vars,e1) e2
 
 variables :: Parser [Var]
 variables = do { v <- variable; return [v] } <|> (angles $ commaSep variable)
 
 lambda :: Parser Expr
-lambda = do reserved "fun"
-            vars <- parens $ commaSep variable
-            symbol "->"
-            expr <- expression
-            return $ Lambda vars expr
+lambda = do
+  reserved "fun"
+  vars <- parens $ commaSep variable
+  symbol "->"
+  expr <- expression
+  return $ Lambda vars expr
 
 letrec :: Parser Expr
-letrec = do reserved "letrec"
-            defs <- many fundef
-            reserved "in"
-            e <- expression
-            return $ LetRec defs e
+letrec = do
+  reserved "letrec"
+  defs <- many fundef
+  reserved "in"
+  e <- expression
+  return $ LetRec defs e
 
 elist :: Parser a -> Parser (List a)
 elist a = brackets $ list a
 
 list :: Parser a -> Parser (List a)
-list el = do elems <- commaSep1 el
-             option (L elems) (do symbol "|"
-                                  t <- el
-                                  return $ LL elems t)
+list el = do
+  elems <- commaSep1 el
+  option (L elems) (do symbol "|"
+                       t <- el
+                       return $ LL elems t)
 
 modcall :: Parser Expr
-modcall = do reserved "call"
-             e1 <- expression
-             symbol ":"
-             e2 <- expression
-             eN <- parens $ commaSep expression
-             return $ ModCall (e1, e2) eN
+modcall = do
+  reserved "call"
+  e1 <- expression
+  symbol ":"
+  e2 <- expression
+  eN <- parens $ commaSep expression
+  return $ ModCall (e1,e2) eN
 
 op :: Parser Expr
-op = do reserved "primop"
-        a <- atom
-        e <- parens $ commaSep expression
-        return $ PrimOp a e
+op = do
+  reserved "primop"
+  a <- atom
+  e <- parens $ commaSep expression
+  return $ PrimOp a e
 
 receive :: Parser Expr
-receive = do reserved "receive"
-             alts <- many $ annotated clause
-             to <- timeout
-             return $ Rec alts to
+receive = do
+  reserved "receive"
+  alts <- many $ annotated clause
+  to <- timeout
+  return $ Rec alts to
 
 timeout :: Parser TimeOut
-timeout = do reserved "after"
-             e1 <- expression
-             symbol "->"
-             e2 <- expression
-             return $ TimeOut  e1 e2
+timeout = do
+  reserved "after"
+  e1 <- expression
+  symbol "->"
+  e2 <- expression
+  return $ TimeOut e1 e2
 
 eseq :: Parser Expr
-eseq =  do reserved "do"
-           e1 <- expression
-           e2 <- expression
-           return $ Seq e1 e2
+eseq = do
+  reserved "do"
+  e1 <- expression
+  e2 <- expression
+  return $ Seq e1 e2
 
 etry :: Parser Expr
-etry = do reserved "try"
-          e1 <- expression
-          reserved "of"
-          v1 <- variables
-          symbol "->"
-          e2 <- expression
-          reserved "catch"
-          v2 <- variables
-          symbol "->"
-          _ <- expression
-          return $ Try e1 (v1,e1) (v2,e2)
+etry = do
+  reserved "try"
+  e1 <- expression
+  reserved "of"
+  v1 <- variables
+  symbol "->"
+  e2 <- expression
+  reserved "catch"
+  v2 <- variables
+  symbol "->"
+  _ <- expression
+  return $ Try e1 (v1,e1) (v2,e2)
 
 tuple :: Parser a -> Parser [a]
 tuple el = braces $ commaSep el
 
 annotation :: Parser [Const]
-annotation = do symbol "-|"
-                cs <- brackets $ many constant
-                return $ cs
+annotation = do
+  symbol "-|"
+  cs <- brackets $ many constant
+  return $ cs
 
 annotated :: Parser a -> Parser (Ann a)
 annotated p = parens (do e <- p
@@ -374,12 +396,13 @@ whiteSpace :: Parser ()
 whiteSpace = Token.whiteSpace lexer
 
 runLex :: Show a => Parser a -> String -> IO ()
-runLex p file = do input <- readFile file
-                   parseTest (do whiteSpace
-                                 x <- p
-                                 eof
-                                 return x) input
-                   return ()
+runLex p file = do
+  input <- readFile file
+  parseTest (do whiteSpace
+                x <- p
+                eof
+                return x) input
+  return ()
 
 -- | Parse of a string, which should contain a complete CoreErlang module
 parseModule :: String -> Either ParseError (Ann Module)
