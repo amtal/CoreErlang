fe3e0c1133a34e6f7e371ce01135a9b3e319bc4a
sdzx1993 <shangdizhixia1993@gmail>
2020-03-25 13:13:17 +0800

    finish add-test-case

--- test/TestParser.hs
+++ test/TestParser.hs
@@ -1,44 +1,52 @@
 module TestParser where
 
-import Test.Tasty
-import Test.Tasty.Hspec
-import Language.CoreErlang
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Language.CoreErlang
+import           System.Directory
+import           Test.Tasty
+import           Test.Tasty.Hspec
 
 main :: IO TestTree
 main = testSpec "parser" spec
 
 spec :: Spec
 spec = do
-  context "SimpleParser" $ do
-      specify "should parse the 'Hello' module" $ do
-        r <- parseFile "test/data/Hello.core"
-        case r of
-          Left _ -> return ()
-          Right annMod -> moduleName annMod `shouldBe` (Atom "Hello")
-      specify "should parse the 'Map' module" $ do
-        r <- parseFile "test/data/Map.core"
-        case r of
-          Left _ -> return ()
-          Right annMod -> moduleName annMod `shouldBe` (Atom "Map")
-  context "JsxParser" $ do
-    specify "should parse the 'jsx' module" $ do
-      r <- parseFile "test/data/jsx/jsx.core"
-      case r of
-        Left _ -> return ()
-        Right annMod -> moduleName annMod `shouldBe` (Atom "jsx")
-  context "SaslParser" $ do
-    specify "should parse the 'sasl' module" $ do
-      r <- parseFile "test/data/sasl/sasl.core"
-      case r of
-        Left _ -> return ()
-        Right annMod -> moduleName annMod `shouldBe` (Atom "jsx")
-  context "StdlibParser" $ do
-    specify "should parse the 'stdlib' module" $ do
-      r <- parseFile "test/data/stdlib/array.core"
-      case r of
-        Left _ -> return ()
-        Right annMod -> moduleName annMod `shouldBe` (Atom "jsx")
+  fps <-runIO $ getCoreFiles "."
+  forM_ fps testFun
 
 moduleName (Constr (Module name _ _ _)) = name
-moduleName (Ann (Module name _ _ _) _) = name
+moduleName (Ann (Module name _ _ _) _)  = name
+
+
+testFun :: FilePath -> Spec
+testFun f = do
+  context "testParser" $ do
+    specify f $ do
+      r <- parseFile f
+      case r of
+        Left _       -> return ()
+        Right annMod -> moduleName annMod `shouldBe` (Atom "jsx")
+
+
+isCoreFile :: String -> Bool
+isCoreFile fname = (== "eroc") $ take 4 $ reverse $ fname
+
+
+getCoreFiles :: FilePath -> IO [FilePath]
+getCoreFiles basePath = do
+  list <- listDirectory basePath
+  r <- forM list $ \filePath -> do
+    let tp = basePath ++ "/" ++ filePath
+    res <- doesDirectoryExist tp
+    if res
+      then getCoreFiles tp
+      else if isCoreFile filePath
+              then return [tp]
+                   else return []
+  return $ concat r
+
+
+
+
 
